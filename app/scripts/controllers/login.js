4596bffa51ecd36989e361fab6f6fc2d27d4f8d8
Berjoyner <joyner.amber@gmail.com>
2014-06-18 21:59:11 -0400

    Google Name Showing and a few minor changes

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,6 +11,8 @@ angular.module('paybackApp')
         $scope.err = err ? err + '' : null;
         $rootScope.$apply(function() {
           $rootScope.googleUser = user;
+          $rootScope.googleUserName = user.displayName;
+          $rootScope.googleUserPic = user.picture;
         });
       });
     };
